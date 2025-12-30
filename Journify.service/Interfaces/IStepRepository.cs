d8d3ae8c8ea8a92c91d00366aca0a452ae363246
odinous95 <mjd.thif@icloud.com>
2025-12-30 22:03:30 +0100

    #4 Add IStepRepository interface and reference Journify.core Introduced IStepRepository for async step operations in the service layer. Added a project reference to Journify.core to enable access to core entities such as Step.

---
+++ Journify.service/Interfaces/IStepRepository.cs
@@ -0,0 +1,10 @@
+﻿using Journify.core.Entities;
+
+namespace Journify.service.Interfaces
+{
+    public interface IStepRepository
+    {
+        Task<IEnumerable<Step>> GetAllStepsAsync();
+        Task AddStepAsync(Step step);
+    }
+}
