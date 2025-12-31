c5d4e9eb751bd384d676c2ce9496ecb4927115d4
odinous95 <mjd.thif@icloud.com>
2026-01-01 00:33:04 +0100

    Add AppDbContext with entity DbSets for EF Core
    Introduced AppDbContext in Journify.infrastructure.Data, inheriting from DbContext. Added DbSet properties for User, DailyJourney, and Step entities to enable database management. Included necessary using directives for entity classes and EF Core.

---
+++ Journify.infrastructure/Data/AddDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Journify.core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Journify.infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<DailyJourney> DailyJournies { get; set; }
+        public DbSet<Step> Steps { get; set; }
+
+    }
+}
