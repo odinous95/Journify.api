6e3c8c91d1446bcbac40f021f069b1ad57273617
odinous95 <mjd.thif@icloud.com>
2026-01-01 15:42:56 +0100

    Add StepUsecase implementing IStepService interface
    Introduced the StepUsecase class as a service layer for Step entities. It implements IStepService and delegates operations to an injected IStepRepository, providing async methods for adding and retrieving steps. Added necessary using directives.

---
+++ Journify.service/usecases/StepUsecase.cs
@@ -0,0 +1,23 @@
+﻿using Journify.core.Entities;
+using Journify.service.Interfaces;
+
+namespace Journify.service.usecases
+{
+    public class StepUsecase : IStepService
+    {
+        private readonly IStepRepository _stepRepository;
+        public StepUsecase(IStepRepository stepRepository)
+        {
+            _stepRepository = stepRepository;
+        }
+        public async Task<Step> AddStepAsync(Step step)
+        {
+            return await _stepRepository.AddStepAsync(step);
+        }
+
+        public async Task<IEnumerable<Step>> GetAllStepsAsync()
+        {
+            return await _stepRepository.GetAllStepsAsync();
+        }
+    }
+}
