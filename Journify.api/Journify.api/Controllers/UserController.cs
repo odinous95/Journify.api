b7ca37a2600c7f789d655b74afd12318e13c4662
odinous95 <mjd.thif@icloud.com>
2026-01-02 13:14:03 +0100

    Add UserController with basic CRUD endpoints
    Introduced a new UserController for the API, providing endpoints to get all users, create a user, and get a user by ID. The controller uses IUserService for business logic, supports async operations, and returns appropriate HTTP responses for each case.

---
+++ Journify.api/Journify.api/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using Journify.core.Entities;
+using Journify.service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Journify.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<User>> GetAllUsers()
+        {
+            var users = await _userService.GetAllUsersAsync();
+            if (users is null || !users.Any())
+            {
+                return NotFound("No users found.");
+            }
+            return Ok(users);
+        }
+        [HttpPost]
+        public async Task<ActionResult<User>> CreateUser([FromBody] User user)
+        {
+            if (user == null)
+            {
+                return BadRequest("User data is null.");
+            }
+            var createdUser = await _userService.AddUserAsync(user);
+            return createdUser;
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUserById(Guid id)
+        {
+            var user = await _userService.GetUserById(id);
+            if (user == null)
+            {
+                return NotFound("User not found.");
+            }
+            return Ok(user);
+        }
+    }
+}
