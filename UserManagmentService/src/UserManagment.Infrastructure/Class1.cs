78a922f44f0f45dadb808e1d22b09c11f2ad72b5
odinous95 <mjd.thif@icloud.com>
2026-01-13 22:14:05 +0100

    Add EF Core PostgreSQL support and AppDbContext setup
    Removed unused Class1.cs. Added AppDbContext and AddDbContextFactory for EF Core with PostgreSQL and retry support. Updated project file to include necessary EF Core and Npgsql packages, and removed Journify.core reference.

--- UserManagmentService/src/UserManagment.Infrastructure/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace UserManagment.Infrastructure
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ UserManagmentService/src/UserManagment.Infrastructure/Data/AddDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Journify.core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace StepManagment.infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Steps { get; set; }
+
+
+    }
+}
