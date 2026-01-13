b935efe9ebc801c5c6d5f0ada5bc22d3f962c4a5
odinous95 <mjd.thif@icloud.com>
2026-01-13 22:04:22 +0100

    Add UserServices class for async user CRUD operations
    Added UserServices to UserManagment.service.usecases, implementing async CRUD methods for User entities via IUserRepository. Included required using directives.

---
+++ UserManagmentService/src/UserManagment.service/usecases/UserServices.cs
@@ -0,0 +1,34 @@
+﻿using Journify.core.Entities;
+using UserManagment.service.Interfaces;
+
+namespace UserManagment.service.usecases
+{
+    public class UserServices
+    {
+        private readonly IUserRepository _userRepository;
+        public UserServices(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public async Task<User> AddUserAsync(User user)
+        {
+            return await _userRepository.AddUserAsync(user);
+        }
+        public async Task<IEnumerable<User>> GetAllUsersAsync()
+        {
+            return await _userRepository.GetAllUsersAsync();
+        }
+        public async Task<User> GetUserById(Guid id)
+        {
+            return await _userRepository.GetUserById(id);
+        }
+        public async Task<User> UpdateUserAsync(User user)
+        {
+            return await _userRepository.UpdateUserAsync(user);
+        }
+        public async Task<bool> DeleteUserAsync(Guid id)
+        {
+            return await _userRepository.DeleteUserAsync(id);
+        }
+    }
+}
