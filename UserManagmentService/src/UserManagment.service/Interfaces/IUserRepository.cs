a149bd421bcf968cff63897bcdb61b1223808bc4
odinous95 <mjd.thif@icloud.com>
2026-01-13 21:58:22 +0100

    Add IUserRepository and IUserService interfaces for users
    Removed empty Class1. Introduced IUserRepository and IUserService interfaces with async CRUD methods for User entities, referencing Journify.core.Entities.

---
+++ UserManagmentService/src/UserManagment.service/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Journify.core.Entities;
+
+namespace UserManagment.service.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAllUsersAsync();
+        Task<User> AddUserAsync(User user);
+        Task<User> GetUserById(Guid id);
+        Task<User> UpdateUserAsync(User user);
+        Task<bool> DeleteUserAsync(Guid id);
+    }
+}
