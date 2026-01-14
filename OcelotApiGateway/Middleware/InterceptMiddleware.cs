846b41271d98606b602a9c2e9c70f0122a2b5ea5
odinous95 <mjd.thif@icloud.com>
2026-01-14 13:00:46 +0100

    Add InterceptMiddleware to set Referrer header
    Introduced InterceptMiddleware to set the "Referrer" header to "Api-Gateway" on incoming requests. Registered this middleware in the pipeline before Ocelot. Updated the root endpoint response message to clarify it's from the Ocelot API gateway.

---
+++ OcelotApiGateway/Middleware/InterceptMiddleware.cs
@@ -0,0 +1,11 @@
+﻿namespace OcelotApiGateway.Middleware
+{
+    public class InterceptMiddleware(RequestDelegate next)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            context.Request.Headers["Referrer"] = "Api-Gateway";
+            await next(context);
+        }
+    }
+}
--- OcelotApiGateway/Program.cs
+++ OcelotApiGateway/Program.cs
@@ -1,13 +1,16 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using OcelotApiGateway.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/", () => "Hello World from Ocelot Api gateway!");
 app.MapControllers();
+app.UseMiddleware<InterceptMiddleware>();
 await app.UseOcelot();
 
 app.Run();
