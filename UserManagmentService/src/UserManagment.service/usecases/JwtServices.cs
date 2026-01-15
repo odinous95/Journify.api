61cec739c276db36956b21407a0fe72eef8575d1
odinous95 <mjd.thif@icloud.com>
2026-01-15 22:33:31 +0100

    Add JwtService for JWT token generation
    Implemented JwtService class with IJwtService interface to generate JWT tokens for User entities. Tokens include user ID and email claims, use configuration for key, issuer, and audience, and expire after 30 minutes. Added required using directives for JWT and configuration support.

---
+++ UserManagmentService/src/UserManagment.service/usecases/JwtServices.cs
@@ -0,0 +1,45 @@
+﻿using Journify.core.Entities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using UserManagment.service.Interfaces;
+
+namespace UserManagment.service.usecases
+{
+    public class JwtService : IJwtService
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string CreateToken(User user)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email)
+            };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration["Jwt:Key"])
+            );
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(30),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
