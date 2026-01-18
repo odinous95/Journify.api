cef39a25e3868862da40a9f67057f44e23e69e81
odinous95 <mjd.thif@icloud.com>
2026-01-18 01:09:36 +0100

    IsCompleted in Step entity defaulted to false

--- Journify.core/Entities/Step.cs
+++ Journify.core/Entities/Step.cs
@@ -6,7 +6,7 @@
         public Guid DailyJourneyId { get; set; }
         public string Title { get; set; } = "";
         public string Description { get; set; } = "";
-        public bool IsCompleted { get; set; }
+        public bool IsCompleted { get; set; } = false;
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime LastUpdatedAt { get; set; } = DateTime.UtcNow;
     }
