e6e57769cb476b3e88421084b3fe156ddc82ae33
odinous95 <mjd.thif@icloud.com>
2026-01-27 23:06:14 +0100

    Remove CreateUserDto and LoginUserDto classes
    Deleted the CreateUserDto and LoginUserDto classes, which previously contained required Email and Password properties. These DTOs are no longer present in the codebase.

--- UserManagmentService/src/UserManagment.api/DTOS/CreateUserDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UserManagment.api.DTOS
-{
-    public class CreateUserDto
-    {
-        public required string Email { get; set; }
-        public required string Password { get; set; }
-    }
-}
--- UserManagmentService/src/UserManagment.api/DTOS/LoginUserDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UserManagment.api.DTOS
-{
-    public class LoginUserDto
-    {
-        public required string Email { get; set; }
-        public required string Password { get; set; }
-    }
-}
